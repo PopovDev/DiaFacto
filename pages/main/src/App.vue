a0b914ae553ce8e44eb18e448117cf297da59c1b
Andrey Popov <andreypopov2628007@gmail.com>
2023-02-13 12:38:50 +0000

    Created project for main web page

---
+++ pages/main/src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+</script>
+
+<template>
+  <div class="app">
+    <h1>None</h1>
+  </div>
+</template>
+
+<style scoped>
+.app {
+  background-color: #000;
+  color: #fff;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
